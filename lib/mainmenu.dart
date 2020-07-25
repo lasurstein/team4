e4148ae3ecd423d75bd332072d76d512f3514eb6
上村奎斗 <kamimura.keito.ry@alumni.tsukuba.ac.jp>
2020-07-25 15:57:51 +0900

    gitのテストとしてmainの作成

---
+++ lib/mainmenu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainAppMenu extends StatefulWidget {
+  @override
+  _MainAppMenuState createState() => _MainAppMenuState();
+}
+
+class _MainAppMenuState extends State<MainAppMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
